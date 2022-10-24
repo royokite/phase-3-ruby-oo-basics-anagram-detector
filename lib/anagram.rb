0f675424ec98d8e1ad0dc0e28370b65e5f70559f
RoyOkite <royokite@gmail.com>
2022-10-24 15:17:23 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+    def initialize word
+        @word = word
+    end
+
+    def match word_array
+        match_array = []
+        word_array.select do |e|
+            if e.chars.sort == @word.chars.sort
+                match_array << e
+            end
+        end
+        p match_array
+    end
+end
+
+word = Anagram.new("allergy")
+word.match(%w[gallery ballerina regally clergy largely leading])
